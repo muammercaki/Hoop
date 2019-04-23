323a80bf50101961a69078d44391ff1a1508e36b
Muammer CAKI <caki.muammer@hotmail.com>
2019-04-23 16:19:57 +0300

    Status page and calls page added dummy data

---
+++ app/src/main/java/io/androidedu/hoop/dao/ChatsDao.kt
@@ -0,0 +1,27 @@
+package io.androidedu.hoop.dao
+
+import androidx.room.*
+import io.androidedu.hoop.entity.ChatsEntity
+
+
+@Dao
+interface ChatsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)             //Ekleme
+    fun addNewItem(chatsDao: ChatsEntity)
+
+    @Delete
+    fun removeItem(chatsDao: ChatsEntity)           //Remove
+
+    @Update
+    fun updateItem(chatsDao: ChatsEntity)    //Update
+
+    @Query("SELECT * FROM chats_table WHERE userProfilPhoto LIKE :id ")
+    fun findSingleItem(id: Int): ChatsEntity
+
+    @Query("SELECT * FROM chats_table")              //tüm Tablo
+    fun getAllList(): List<ChatsEntity>
+
+    @Query("DELETE  FROM chats_table")    //Delete tüm tablo
+    fun deleteAllTableList()
+}
